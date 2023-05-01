bb95899d1be4fa5660c2050eb7d24d2a575b28d9
SinoLewis <sinolewis@gmail.com>
2023-05-01 17:21:55 +0300

    Hello world without cors

--- supabase/functions/browser-with-cors/index.ts
+++ supabase/functions/browser-with-cors/index.ts
@@ -2,34 +2,34 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from 'std/server'
-import { corsHeaders } from '../_shared/cors.ts'
+import { serve } from "std/server";
+import { corsHeaders } from "../_shared/cors.ts";
 
-console.log(`Function "browser-with-cors" up and running!`)
+console.log(`Function "browser-with-cors" up and running!`);
 
 serve(async (req) => {
   // This is needed if you're planning to invoke your function from a browser.
-  if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders })
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
   }
 
   try {
-    const { name } = await req.json()
+    const { name } = await req.json();
     const data = {
       message: `Hello ${name}!`,
-    }
+    };
 
     return new Response(JSON.stringify(data), {
-      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
       status: 200,
-    })
+    });
   } catch (error) {
     return new Response(JSON.stringify({ error: error.message }), {
-      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
       status: 400,
-    })
+    });
   }
-})
+});
 
 // To invoke:
 // curl -i --location --request POST 'http://localhost:54321/functions/v1/browser-with-cors' \
